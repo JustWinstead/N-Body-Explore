9a244207fac04921fc13b270512e98f413a2a49c
Kawiggles <kawikadembroski@gmail.com>
2026-05-03 18:45:11 -0600

    integrator written
    should work with new version of force vector calculations

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
+# Definitions:
+#   System: list of bodies
+#   Body: class object consisting of physical properties, ie mass, position, volume
+
 import numpy as np
 import math
 G = 6.6743e-11 # Grav constant: 6.6743 x 10^(-11) m^3/(kg * s^2)
 
 # Body object to be used in equations
 class Body:
-    def __init__(self, name, mass, pos, velo, accel):
+    def __init__(self, name, mass, pos, velo):
         self.name = name
         self.mass = mass
         self.pos = pos
         self.velo = velo
-        self.accel = accel
     force = 0
-
-# Newtonian accleration
-#   system is a list (or some other data structure) of body objects   
-#   i is the specific object we want to get the acceleration of
-def newtAcceleration(system, i):
-    accel = np.zeros(3) # Array to represent acceleration vector
-    for j in system:
-        if j == i:
-            continue # Guard to prevent divide by zero (if qi = qj, then diff = 0)
-        # defining placeholder variables is for cowards
-        #F = ma -> a = F/m which cancels its own mass out
-        accel += ( G * j.mass ) / (np.linalg.norm(j.pos - i.pos)**2 )
-    return accel
+    accel = 0
 
 def magnitude(vec):
-    '''
-    The input vector should be an array with x,y,z components
-    '''
     return np.sqrt(np.sum(vec**2))
     
 def gravForceMagnitude(i, j):
@@ -36,30 +24,36 @@ def gravForceMagnitude(i, j):
     return  G * i.mass * j.mass / (separation)**2
     
 def unitDirectionVector(i,j):
-    '''
-    
-    '''
     direc = j.pos-i.pos
     unit_vec = direc/magnitude(direc)
     return unit_vec
+
 def gravForceVector(i,j):
-    '''
-    
-    '''
     separation = j.pos-i.pos
     force = gravForceMagnitude(i,j)
     direction = unitDirectionVector(i,j)
     return force*direction
 
-def calculateForceVector(system):
-    calculatedVectors = {}
-
-    for i in range(len(system)):
-        for j in range(len(range(system)):
+def calculateForceVectors(system):
+    calculatedForces = {}
+    for i in system:
+        for j in system:
             if i != j:
                 hashname = hash("".join(sorted(i.name + j.name)))
-                if hashname in calculatedVectors:
-                    i.force += calculatedVectors[hashname] * -1
+                if hashname in calculatedForces:
+                    i.force += calculatedForces[hashname] * -1
                 else:
                     i.force += gravForceVector(i, j)
-                    calculatedVectors[hashname] = i.force
+                    calculatedForces[hashname] = i.force
+
+def integrator(system, dt):
+    calculateForceVectors(system)
+
+    for i in system:
+        i.velo += 0.5 * ((i.force/i.mass) + i.accel) * dt
+        i.accel = i.force/i.mass
+        i.pos += i.velo * dt + 0.5 * i.accel * dt**2
+
+# TODO: write function which generates body from system where the pos is the center of mass of the system 
+# Hard part: figuring out when to abstract and unabstract systems
+
